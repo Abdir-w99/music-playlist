29d27da5bf012bb20839a482e020bbcda826683c
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-02-15 19:22:11 +0100

    Add initial playlists data

---
+++ script.js
@@ -0,0 +1,19 @@
+let playList = [
+  {
+    id: "workout",
+    name: "Workout",
+    tracks: [],
+  },
+
+  {
+    id: "chill",
+    name: "Chill",
+    tracks: [],
+  },
+
+  {
+    id: "party",
+    name: "party",
+    tracks: [],
+  },
+];
